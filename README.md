5a4d63ca31a3f8e2e7a5818a77b1ac108afcf0b1
hsinjung wu <hsinjungwu@gmail.com>
2015-06-03 16:18:35 +0800

    First Publish
    first publish!!

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # ResetTabOrder
-Reset Control Tab Index for Windows Form
+
+This is an Add-in for *Visual Studio 2010*
+
+You can use it to reset all controls' tab index on a Windows Form.
+
+##How to Install
+
+1. Download [latest version Add-in](https://github.com/hsinjungwu/ResetTabOrder/releases/download/v1.0.0/ResetTabOrder.zip) 
+2. Extact it to `%userprofile%\Documents\Visual Studio 2010\Addins`
+3. Open Your Visual Studio and Enjoy it!!
+
+##How to use
+
+Step 1 : random order
+
+![before](.\tutorial\before.png)
+
+Step 2 : click *ResetTabOrder*
+
+![menu](.\tutorial\menu.png)
+
+Step 3 : pop out dialog and press 【YES】 or 【Yes to All】
+
+![menu](.\tutorial\dialog.png)
+
+Step 4 : well order
+
+![menu](.\tutorial\after.png)
+
+----
+
+##功能：
+
+為自動調整畫面控制項的 Tab Index 的順序，其中順序為從最左上角開始，接著依序等高[^1][^2]向右。如果右邊沒有控制項的話就會跳到下一行。如果你的順序不是這樣，請自行手動調整
+
+##安裝：
+
+1. 下載[最新版本 Add-in](https://github.com/hsinjungwu/ResetTabOrder/releases/download/v1.0.0/ResetTabOrder.zip) 
+
+2. 解壓縮後到 `%userprofile%\Documents\Visual Studio 2010\Addins`。
+
+3. 打開你的 Visual Studio 就可以看到它已經安裝好了。
+
+##使用方法：
+
+Step 1 : Form 上原先是亂七八糟的排序
+
+![before](.\tutorial\before.png)
+
+Step 2 : 點選 ResetTabOrder
+
+![menu](.\tutorial\menu.png)
+
+Step 3 : 跳出修改後的結果，請按【是】或【全部皆是】
+
+![menu](.\tutorial\dialog.png)
+
+Step 4 : 已排序完的結果
+
+![menu](.\tutorial\after.png) 
+
+[^1]: 請多利用 Visual Studio 的自動對齊及等寬或等高功能。[before](.\tutorial\align.png)
+
+[^2]: 不管是用對齊上緣、中間或是下緣，這個 Add-in 都能 work
---
+++ project/ResetTabOrder/ResetTabOrder.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using EnvDTE;
+using EnvDTE80;
+
+namespace ResetTabOrder
+{
+    /// <summary>
+    /// 重設 Tab Index 順序
+    /// </summary>
+    public static class ResetTabOrder
+    {
+        private static Dictionary<Control, int> allControls = new Dictionary<Control, int>();
+
+        /// <summary>
+        /// 執行重設
+        /// </summary>
+        /// <param name="activeDoc">目前執行畫面</param>
+        public static void DoWork(Document activeDoc)
+        {
+            allControls.Clear();
+
+            IDesignerHost host = (IDesignerHost)activeDoc.ActiveWindow.Object;
+            IComponent cf = host.RootComponent;
+
+            bool isForm = cf is Form;
+            if (!isForm) MessageBox.Show(".....");
+
+            Form f = cf as Form;
+            ResetSubControls(f);
+
+            WriteToDesign(activeDoc.FullName);
+
+        }
+
+        private static void ResetSubControls(Control parentCtrl)
+        {
+            List<Control> subCtrl = new List<Control>();
+
+            foreach (Control sbCtrl in parentCtrl.Controls)
+            {
+                subCtrl.Add(sbCtrl);
+                allControls.Add(sbCtrl, sbCtrl.TabIndex);
+            }
+
+            Control[] reorderSubCtrl
+                = subCtrl.AsEnumerable()
+                         .OrderBy(c => c.Location.Y)
+                         .ThenBy(c => c.Location.X)
+                         .ToArray();
+
+            for (int i = 0; i < reorderSubCtrl.Length; i++)
+            {
+                Control c = reorderSubCtrl[i];
+                c.TabIndex = i;
+                ResetSubControls(c);
+            }
+        }
+
+        private static void WriteToDesign(string docPath)
+        {
+            string designerPath =
+                Path.ChangeExtension(docPath, ".Designer.cs");
+
+            string designContent = File.ReadAllText(designerPath);
+
+            foreach (var c in allControls)
+            {
+                string oldSetting = string.Format("{0}.TabIndex = {1};", c.Key.Name, c.Value);
+                string newSetting = string.Format("{0}.TabIndex = {1};", c.Key.Name, c.Key.TabIndex);
+                designContent = designContent.Replace(oldSetting, newSetting);
+            }
+
+            File.WriteAllText(designerPath, designContent);
+        }
+    }
+}
